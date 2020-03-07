7d70c9b70e96af17dd712d6f32a512dda9aecb06
qbroquetas <qbroquetas@outlook.com>
2020-03-07 14:56:48 +0100

    add 4th game and scores

--- crearBBDD.sql
+++ crearBBDD.sql
@@ -34,6 +34,7 @@ insert into jugadors values (4, 'Laia', 'Bcn92');
 insert into partides values (1, '2020-01-03 10:02:32', '2020-01-03 10:32:42');
 insert into partides values (2, '2020-01-04 12:42:32', '2020-01-04 13:03:34');
 insert into partides values (3, '2020-01-05 23:34:46', '2020-01-05 23:55:02');
+insert into partides values (4, '2020-01-06 21:34:46', '2020-01-06 22:12:03');
 
 insert into participants values (1, 1, 'Babo_1', 1000);
 insert into participants values (3, 1, 'Babo_1', 2000);
@@ -42,15 +43,12 @@ insert into participants values (4, 1, 'Babo_2', 3000);
 insert into participants values (2, 2, 'Babo_3', 2000);
 insert into participants values (4, 2, 'Babo_2', 4000);
 
-
 insert into participants values (1, 3, 'Babo_3', 2000);
 insert into participants values (2, 3, 'Babo_3', 2000);
 insert into participants values (3, 3, 'Babo_1', 3500);
-insert into participants values (4, 3, 'Babo_2', 2500);
+insert into participants values (4, 3, 'Babo_2', 4500);
 
-
-
-
-
-	
-	
+insert into participants values (1, 4, 'Babo_1', 500);
+insert into participants values (2, 4, 'Babo_2', 2500);
+insert into participants values (3, 4, 'Babo_2', 1500);
+insert into participants values (4, 4, 'Babo_3', 1100);
